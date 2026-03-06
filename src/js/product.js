b0e82d43d10c88152599682b15676f5212439f29
Dylan Cleghorn <dylan.cleghorn@gmail.com>
2026-03-06 12:05:46 -0600

    updated addProductToCart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,14 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs'; // getLocalStorage to pull existing cart items 
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cartItems = getLocalStorage('so-cart') || []; // get existing cart items
+  cartItems.push(product); // add cart items
+  setLocalStorage('so-cart', cartItems); // push back to storage
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
