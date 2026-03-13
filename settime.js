a6d6370f4df86d8dd090ee718571fadbe9294f3a
dev-medina29 <mmbedi@byupathway.edu>
2026-03-13 09:42:17 +0100

    save this new update

---
+++ settime.js
@@ -0,0 +1,13 @@
+let t = document.getElementById("countdown");
+let countdown = 10;
+let button = document.getElementById("startButton");
+button.addEventListener("click", () => {
+  let timer = setInterval(() => {
+    countdown--;
+    t.innerHTML = `${countdown} seconds`;
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(timer);
+    t.innerHTML = "Time's up!";
+  }, countdown * 1000);
+});
