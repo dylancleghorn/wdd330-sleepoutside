45a57e988f27deed13db5976ae9808d9bedcd9e7
dev-medina29 <mmbedi@byupathway.edu>
2026-03-20 09:06:20 +0100

    save

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { LoadHeaderFooter } from "./utils.mjs";
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const productList = new ProductList("Tents", dataSource, element);
 
-const dataSource = new ProductData('tents');
-const element = document.querySelector('.product-list');
-const productList = new ProductList('Tents', dataSource, element);
-
-productList.init();
+LoadHeaderFooter().then(() => {
+  productList.init();
+});
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,7 +7,7 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key)) || []; // added empty array when null to avoid error on cart page 
+  return JSON.parse(localStorage.getItem(key)) || []; // added empty array when null to avoid error on cart page
 }
 
 // save data to local storage
@@ -29,14 +29,42 @@ export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(param);
-  return product
+  return product;
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false,
+) {
   const htmlStrings = list.map(templateFn);
   // if clear is true we need to clear out the contents of the parent.
   if (clear) {
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+async function loadTemplate(path) {
+  const response = await fetch(path);
+  return await response.text();
+}
+export async function LoadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../public/partials/header.html");
+
+  const headerElement = document.querySelector("#main-header");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  const footerTemplate = await loadTemplate("../public/partials/footer.html");
+
+  const footerElement = document.querySelector("#main-footer");
+  renderWithTemplate(footerTemplate, footerElement);
 }
