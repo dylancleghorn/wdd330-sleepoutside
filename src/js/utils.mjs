128c311732558c4267bee59cae896fd07f6fc2b3
dev-medina29 <mmbedi@byupathway.edu>
2026-03-20 08:09:32 +0100

    save

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,12 +7,14 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key)) || []; // added empty array when null to avoid error on cart page
 }
+
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -20,4 +22,49 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
+}
+
+// get the product id from the query string
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false,
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+async function loadTemplate(path) {
+  const response = await fetch(path);
+  return await response.text();
+}
+export async function LoadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+
+  const headerElement = document.querySelector("#main-header");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const footerElement = document.querySelector("#main-footer");
+  renderWithTemplate(footerTemplate, footerElement);
 }
