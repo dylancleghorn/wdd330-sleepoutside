8ccb7108f5fa7ea5f2bbb005f6bf8ea1119a2239
.\.git <.\.gitignore>
2026-03-21 21:53:38 -0300

    loadheaderfooter

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,11 @@
 import { getParam, loadHeaderFooter } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
+import { initNewsletter } from './newsletter.mjs';
 
 async function init() {
   await loadHeaderFooter();
+  initNewsletter();
 
   const dataSource = new ProductData('tents');
   const productID = getParam('product');
