9b32107554e5beb473eeedadd4cd0d94baa70d92
dev-medina29 <mmbedi@byupathway.edu>
2026-03-22 17:45:06 +0100

    save : individual_task_week3

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,10 +1,15 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { LoadHeaderFooter } from "./utils.mjs";
-const dataSource = new ProductData("tents");
+import { LoadHeaderFooter, getParam } from "./utils.mjs";
+const category = getParam("category");
+const dataSource = new ProductData(category);
 const element = document.querySelector(".product-list");
-const productList = new ProductList("Tents", dataSource, element);
+const productList = new ProductList(category, dataSource, element);
 
 LoadHeaderFooter().then(() => {
+  const title = document.querySelector(".title");
+  if (title) {
+    title.textContent = `Top Products: ${category}`;
+  }
   productList.init();
 });
